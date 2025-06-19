ec34a5f2c3e50dd76e5dafd82319367cae6363bf
Vishal Kumar <wwwvishalkumar872@gmail.com>
2025-06-19 21:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+  n = parseInt(n);
+  let count = 0;
+  while (n >= 5) {
+    n = Math.floor(n / 5);
+    count += n;
+  }
+  return count;
 }
 
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
+
+
